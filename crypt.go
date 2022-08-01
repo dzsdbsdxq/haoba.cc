ef3167371d257d6487279b261796f379b5e0b1a5
李大壮 <108051725+dzsdbsdxq@users.noreply.github.com>
2022-08-01 17:24:34 +0800

    Update crypt.go

--- crypt.go
+++ crypt.go
@@ -21,12 +21,12 @@ const lockStream = "st=lDEFABCkVWXYZabc89LMmGH012345uvdefIJK6NOPyzghijQRSTUwx7no
 func encrypt(textSteam string, password string) (encodeStr string) {
 	var k int
 	var j int
-
 	stream := []byte(lockStream)
-
+	
 	lockLen := len(stream)
-	rand.Seed(time.Now().UnixNano())
-	lockCount := rand.Intn(lockLen - 1)
+	ran := rand.New(rand.NewSource(time.Now().Unix()))
+	lockCount := ran.Intn(lockLen)
+
 	randomLock := string(stream[lockCount])
 
 	password = func(str string) string {
@@ -45,15 +45,68 @@ func encrypt(textSteam string, password string) (encodeStr string) {
 			return r == ts
 		})
 		j = (j + lockCount + int(password[k])) % (lockLen)
-		encodeStr += string(stream[j])
+		encodeStr = encodeStr + string(stream[j])
 		k++
 	}
+	return encodeStr + randomLock
+}
+
+/**
+ * 可逆解密
+ *
+ * @param  textStream 要解密的字符串
+ * @param  password 加密私钥=解密私钥
+ */
+func decrypt(textSteam string, password string) (decodeStr string) {
+	var k int
+	var j int
+	stream := []byte(lockStream)
+
+	lockLen := len(stream)
+
+	// 截取随机密锁值
+	textLen := len(textSteam)
+	randomLock := textSteam[textLen-1]
+	// 获得随机密码值的位置
+	lockCount := bytes.IndexFunc(stream, func(r rune) bool {
+		return r == rune(randomLock)
+	})
+	password = func(str string) string {
+		h := md5.New()
+		h.Write([]byte(str))
+		return hex.EncodeToString(h.Sum(nil))
+	}(password + string(randomLock))
+
+	// 开始对字符串解密
+	textSteam = textSteam[:textLen-1]
+	for _, ts := range textSteam {
+		if k == len(password) {
+			k = 0
+		}
+		j = bytes.IndexFunc(stream, func(r rune) bool {
+			return r == ts
+		})
+
+		j = j - lockCount - int(password[k])
+
+		for {
+			if j >= 0 {
+				break
+			}
+			j = j + lockLen
+		}
+		decodeStr += string(lockStream[j])
+		k++
+	}
+	//fmt.Println(decodeStr)
+	decodeByte, _ := base64.StdEncoding.DecodeString(decodeStr)
+	decodeStr = string(decodeByte)
 	return
 }
 
 func main() {
 	s := encrypt("dzsdbsdxq", "123456")
 	fmt.Println(s)
-	//来个大佬写解密
-
+	d := decrypt(s, "123456")
+	fmt.Println(d)
 }
